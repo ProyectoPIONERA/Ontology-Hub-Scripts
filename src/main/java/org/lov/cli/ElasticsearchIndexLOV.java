920ba1db78b0310ae5fe78d34671931127fa2f72
Jhon Alexander Rueda Sanchez <ja.rueda@upm.es>
2026-05-11 19:46:55 +0200

    Adding credentials for creating index

--- src/main/java/org/lov/cli/ElasticsearchIndexLOV.java
+++ src/main/java/org/lov/cli/ElasticsearchIndexLOV.java
@@ -2,6 +2,7 @@ package org.lov.cli;
 
 import arq.cmdline.CmdGeneral;
 import com.hp.hpl.jena.tdb.TDBFactory;
+import org.lov.config.ElasticsearchConfig;
 import org.lov.vocidex.VocidexDocument;
 import org.lov.vocidex.VocidexException;
 import org.lov.vocidex.VocidexIndex;
@@ -16,60 +17,56 @@ import java.util.Properties;
 /**
  * A command line tool that indexes an LOV dump, adding all vocabularies
  * and their terms to the index. Uses {@link LOVExtractor}.
- * 
+ *
  * @author Richard Cyganiak
  */
 public class ElasticsearchIndexLOV extends CmdGeneral {
-	private final static Logger log = LoggerFactory.getLogger(ElasticsearchIndexLOV.class);
-	
-	public static void main(String... args) {
-		new ElasticsearchIndexLOV(args).mainRun();
-	}
+    private final static Logger log = LoggerFactory.getLogger(ElasticsearchIndexLOV.class);
 
-	private String clusterName;
-	private String hostName;
-	private String indexName;
-	private String lovDumpFile;
+    public static void main(String... args) {
+        new ElasticsearchIndexLOV(args).mainRun();
+    }
+
+    private ElasticsearchConfig elastic;
+    private String lovDumpFile;
     private String lovTMPDumpPath;
-	
-	public ElasticsearchIndexLOV(String[] args) {
-		super(args);
-		getUsage().startCategory("Arguments");
-		getUsage().addUsage("configFilePath", "absolute path for the configuration file  (e.g. /home/...)");
-	}
-	
-	@Override
+
+    public ElasticsearchIndexLOV(String[] args) {
+        super(args);
+        getUsage().startCategory("Arguments");
+        getUsage().addUsage("configFilePath", "absolute path for the configuration file  (e.g. /home/...)");
+    }
+
+    @Override
     protected String getCommandName() { return "index-lov"; }
-	@Override
-	protected String getSummary() { return getCommandName() + " clusterName hostname indexName lov.nq"; }
+    @Override
+    protected String getSummary() { return getCommandName() + " clusterName hostname indexName lov.nq"; }
 
-	@Override
-	protected void processModulesAndArgs() {
-		if (getPositional().size() < 1) {
-			doHelp();
-		}
-		String configFilePath = getPositionalArg(0);
-		//load properties from the config file
-		try {
-			Properties lovConfig = new Properties();
-			File file = new File(configFilePath);
-			InputStream is = new FileInputStream(file);
-			lovConfig.load(is);
-			hostName= lovConfig.getProperty("ELASTICSEARCH_HOST");
-			clusterName = lovConfig.getProperty("ELASTICSEARCH_CLUSTER");
-			indexName = lovConfig.getProperty("ELASTICSEARCH_INDEX_NAME");
-			lovDumpFile = lovConfig.getProperty("LOV_NQ_FILE_PATH");
+    @Override
+    protected void processModulesAndArgs() {
+        if (getPositional().size() < 1) {
+            doHelp();
+        }
+        String configFilePath = getPositionalArg(0);
+        //load properties from the config file
+        try {
+            Properties lovConfig = new Properties();
+            File file = new File(configFilePath);
+            InputStream is = new FileInputStream(file);
+            lovConfig.load(is);
+            elastic = ElasticsearchConfig.fromProperties(lovConfig);
+            lovDumpFile = lovConfig.getProperty("LOV_NQ_FILE_PATH");
             lovTMPDumpPath = lovConfig.getProperty("LOV_TMP_DUMP_PATH");
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
-	@Override
-	protected void exec() {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void exec() {
 
         try {
             log.info("Loading LOV dump into TDB (disk): " + lovDumpFile);
@@ -92,11 +89,12 @@ public class ElasticsearchIndexLOV extends CmdGeneral {
                 tripleCount += dataset.getNamedModel(aux).size();
             }
             log.info("Read " + tripleCount + " triples in " + graphCount + " graphs");
-            log.info("Hostname: " + hostName);
-            VocidexIndex index = new VocidexIndex(clusterName, hostName, indexName);
+            log.info("Hostname: " + elastic.hostName);
+            VocidexIndex index = new VocidexIndex(elastic.clusterName, elastic.hostName, elastic.indexName,
+                    elastic.user, elastic.password);
             try {
                 if (!index.exists()) {
-                    throw new VocidexException("Index '" + indexName + "' does not exist on the cluster. Create the index first!");
+                    throw new VocidexException("Index '" + elastic.indexName + "' does not exist on the cluster. Create the index first!");
                 }
 
                 /* Process Agents */
--- src/main/java/org/lov/vocidex/VocidexIndex.java
+++ src/main/java/org/lov/vocidex/VocidexIndex.java
@@ -18,127 +18,131 @@ import org.elasticsearch.client.RestClientBuilder;
 
 /**
  * A connection to a specific named index on an ElasticSearch cluster
- * 
+ *
  * @author Richard Cyganiak
  */
 public class VocidexIndex implements Closeable {
-	private final String hostName;
-	private final String indexName;
-	private final String password;
-	private ElasticsearchClient client = null;
-	private RestClient restClient = null;
-	
-	public VocidexIndex(String clusterName, String hostName, String indexName) {
-		this.hostName = hostName;
-		this.indexName = indexName;
-		this.password = "OntologyHub2026"; // Default password from configuration
-	}
-	
-	public VocidexIndex(String clusterName, String hostName, String indexName, String password) {
-		this.hostName = hostName;
-		this.indexName = indexName;
-		this.password = password;
-	}
+    private final String hostName;
+    private final String indexName;
+    private final String userName;
+    private final String password;
+    private ElasticsearchClient client = null;
+    private RestClient restClient = null;
 
-	/**
-	 * Connects to the cluster if not yet connected. Is called implicitly by
-	 * all operations that require a connection. 
-	 * @throws IOException 
-	 */
-	public void connect() throws IOException {
-		if (client != null) return;
-		
-		final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-		credentialsProvider.setCredentials(AuthScope.ANY, 
-			new UsernamePasswordCredentials("elastic", password));
+    public VocidexIndex(String clusterName, String hostName, String indexName) {
+        this(clusterName, hostName, indexName, "elastic", "OntologyHub2026");
+    }
 
-		RestClientBuilder builder = RestClient.builder(new HttpHost(hostName, 9200))
-			.setHttpClientConfigCallback(httpClientBuilder -> 
-				httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+    public VocidexIndex(String clusterName, String hostName, String indexName, String password) {
+        this(clusterName, hostName, indexName, "elastic", password);
+    }
 
-		restClient = builder.build();
-		RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
-		client = new ElasticsearchClient(transport);
-	}
-	
-	public void close() {
-		try {
-			if (restClient != null) {
-				restClient.close();
-			}
-		} catch (IOException e) {
-			// Ignore close errors
-		}
-		restClient = null;
-		client = null;
-	}
-	
-	public boolean exists() throws IOException {
-		connect();
-		return client.indices().exists(exists -> exists.index(indexName)).value();
-	}
-	
-	public void delete() throws IOException {
-		connect();
-		client.indices().delete(delete -> delete.index(indexName));		
-	}
-	
-	public boolean create() throws IOException {
-		connect();
-		
-		// Read settings from the new mappings location
-		String settings = JSONHelper.readFile("/Users/alexel200/Downloads/Pionera/Ontology-Hub/app/elastic/mappings/settings.json");
-		
-		// Create index with specific settings
-		client.indices().create(create -> 
-			create.index(indexName)
-				.withJson(new java.io.StringReader(settings))
-		);
-		
-		System.out.println("Index created with settings");
-		
-		// Apply mappings using new paths
-		String[] mappings = {
-			"class", "property", "datatype", "instance", "vocabulary", 
-			"person", "organization", "individual"
-		};
-		
-		for (String mapping : mappings) {
-			String mappingPath = "/Users/alexel200/Downloads/Pionera/Ontology-Hub/app/elastic/mappings/" + mapping + ".json";
-			if (!setMapping(mapping, mappingPath)) {
-				return false;
-			}
-			System.out.println("Mapping applied for: " + mapping);
-		}
-		
-		return true;
-	}
-	
-	public boolean setMapping(String type, String jsonConfigFile) {
-		try {
-			connect();
-			String json = JSONHelper.readFile(jsonConfigFile);
-			client.indices().putMapping(put -> 
-				put.index(indexName)
-					.withJson(new java.io.StringReader(json))
-			);
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	/**
-	 * Adds a document (that is, a JSON structure) to the index.
-	 * @return The document's id
-	 */
-	public String addDocument(VocidexDocument document) throws IOException {
-		connect();
-		return client.index(idx -> 
-			idx.index(indexName)
-				.id(document.getId())
-				.withJson(new java.io.StringReader(document.getJSONContents()))
-		).id();
-	}
+    public VocidexIndex(String clusterName, String hostName, String indexName, String userName, String password) {
+        this.hostName = hostName;
+        this.indexName = indexName;
+        this.userName = userName != null ? userName : "elastic";
+        this.password = password != null ? password : "";
+    }
+
+    /**
+     * Connects to the cluster if not yet connected. Is called implicitly by
+     * all operations that require a connection.
+     * @throws IOException
+     */
+    public void connect() throws IOException {
+        if (client != null) return;
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(userName, password));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostName, 9200))
+                .setHttpClientConfigCallback(httpClientBuilder ->
+                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+        restClient = builder.build();
+        RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        client = new ElasticsearchClient(transport);
+    }
+
+    public void close() {
+        try {
+            if (restClient != null) {
+                restClient.close();
+            }
+        } catch (IOException e) {
+            // Ignore close errors
+        }
+        restClient = null;
+        client = null;
+    }
+
+    public boolean exists() throws IOException {
+        connect();
+        return client.indices().exists(exists -> exists.index(indexName)).value();
+    }
+
+    public void delete() throws IOException {
+        connect();
+        client.indices().delete(delete -> delete.index(indexName));
+    }
+
+    public boolean create() throws IOException {
+        connect();
+
+        // Read settings from the new mappings location
+        String settings = JSONHelper.readFile("/Users/alexel200/Downloads/Pionera/Ontology-Hub/app/elastic/mappings/settings.json");
+
+        // Create index with specific settings
+        client.indices().create(create ->
+                create.index(indexName)
+                        .withJson(new java.io.StringReader(settings))
+        );
+
+        System.out.println("Index created with settings");
+
+        // Apply mappings using new paths
+        String[] mappings = {
+                "class", "property", "datatype", "instance", "vocabulary",
+                "person", "organization", "individual"
+        };
+
+        for (String mapping : mappings) {
+            String mappingPath = "/Users/alexel200/Downloads/Pionera/Ontology-Hub/app/elastic/mappings/" + mapping + ".json";
+            if (!setMapping(mapping, mappingPath)) {
+                return false;
+            }
+            System.out.println("Mapping applied for: " + mapping);
+        }
+
+        return true;
+    }
+
+    public boolean setMapping(String type, String jsonConfigFile) {
+        try {
+            connect();
+            String json = JSONHelper.readFile(jsonConfigFile);
+            client.indices().putMapping(put ->
+                    put.index(indexName)
+                            .withJson(new java.io.StringReader(json))
+            );
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Adds a document (that is, a JSON structure) to the index.
+     * @return The document's id
+     */
+    public String addDocument(VocidexDocument document) throws IOException {
+        connect();
+        return client.index(idx ->
+                idx.index(indexName)
+                        .id(document.getId())
+                        .withJson(new java.io.StringReader(document.getJSONContents()))
+        ).id();
+    }
 }
